846f8cc330aad8b6427a2023b18ad2b337b19d44
Tafi <tafizero2@gmail.com>
2026-09-17 23:26:02 +0700

    chore(android): bump version to 1.0.15 (versionCode 27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.futaland.realestate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.0.14"
+        versionCode = 27
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
