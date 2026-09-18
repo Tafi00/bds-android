931a42ab972d3e1b1105ef9053cc6daf2b19b740
Tafi00 <tafizero2@gmail.com>
2026-09-18 13:47:36 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.futaland.realestate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
+        versionCode = 28
         versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
