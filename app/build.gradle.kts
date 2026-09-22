145a4ce87f0d14e1c82040cdb054ba3e7dbdb00b
Tafi <tafizero2@gmail.com>
2026-09-22 22:38:53 +0700

    chore(release): bump versionCode to 46 for Google Play Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.futaland.realestate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 45
+        versionCode = 46
         versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
