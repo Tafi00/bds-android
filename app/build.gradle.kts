1c92e55b0169602e56041cb532bef112a7812a46
Tafi00 <tafizero2@gmail.com>
2026-09-24 17:29:33 +0700

    chore: bump 1.1.4 (49)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.futaland.realestate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.1.3"
+        versionCode = 49
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
