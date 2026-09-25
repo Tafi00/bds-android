5754a325c07f17f6d1520fb5b8d052a3a68f5102
Tafi00 <tafizero2@gmail.com>
2026-09-25 18:30:20 +0700

    chore(release): bump version to 1.1.6 (51)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.futaland.realestate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50
-        versionName = "1.1.5"
+        versionCode = 51
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
